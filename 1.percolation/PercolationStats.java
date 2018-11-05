f6dbcddd33a1cdcdbfe73cb0f96f36075dce14fa
Biyun Wu <beenwu2016@gmail.com>
2018-11-04 22:53:35 -0500

    Add PercolationStats class

---
+++ 1.percolation/PercolationStats.java
@@ -0,0 +1,37 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+public class PercolationStats {
+    // perform trials independent experiments on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0)
+            throw new java.lang.IllegalArgumentException("All inputs should greater than 0.");
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
